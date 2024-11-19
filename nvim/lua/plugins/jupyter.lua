5fe004b445dbf30e2e9b6ece4c7969b8182308e3
mtoon <77199166+mtoons@users.noreply.github.com>
2024-11-19 14:10:10 +0100

    jupyter

--- nvim/lua/plugins/jupyter.lua
+++ nvim/lua/plugins/jupyter.lua
@@ -1,25 +1,85 @@
 return {
     {
-        "benlubas/molten-nvim",
-        lazy = true,
-        event = "BufEnter *.ipynb",
-        version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
-        dependencies = { "willothy/wezterm.nvim" },
-        build = ":UpdateRemotePlugins",
-        init = function()
-            -- these are examples, not defaults. Please see the readme
-            vim.g.molten_output_win_max_height = 20
-            vim.g.molten_auto_open_output = false -- cannot be true if molten_image_provider = "wezterm"
-            vim.g.molten_output_show_more = true
-            vim.g.molten_image_provider = "wezterm"
-            vim.g.molten_output_virt_lines = true
-            vim.g.molten_split_direction =
-            "right"                      --direction of the output window, options are "right", "left", "top", "bottom"
-            vim.g.molten_split_size = 40 --(0-100) % size of the screen dedicated to the output window
-            vim.g.molten_virt_text_output = true
-            vim.g.molten_use_border_highlights = true
-            vim.g.molten_virt_lines_off_by_1 = true
-            vim.g.molten_auto_image_popup = false
+        'Vigemus/iron.nvim',
+        config = function()
+            local iron = require("iron.core")
+
+            iron.setup {
+                config = {
+                    -- Whether a repl should be discarded or not
+                    scratch_repl = true,
+                    -- Your repl definitions come here
+                    repl_definition = {
+                        sh = {
+                            -- Can be a table or a function that
+                            -- returns a table (see below)
+                            command = { "sh" }
+                        },
+                        python = {
+                            -- command = { "wsl", "python3" }, -- or { "python3", "ipython", "--no-autoindent" }
+                            command = { "pwsh", "-Command", "python3" },
+                            format = require("iron.fts.common").bracketed_paste_python
+                        },
+                        markdown = {
+                            -- command = { "wsl", "python3" }, -- or { "python3", "ipython", "--no-autoindent" }
+                            command = { "pwsh", "-Command", "python3" },
+                            format = require("iron.fts.common").bracketed_paste_python
+                        },
+                    },
+                    -- How the repl window will be displayed
+                    -- See below for more information
+                    repl_open_cmd = require("iron.view").split.vertical.botright("40%", {
+                        winfixwidth = false,
+                        winfixheight = false,
+                        -- any window-local configuration can be used here
+                        number = false
+                    })
+                },
+                -- Iron doesn't set keymaps by default anymore.
+                -- You can set them here or manually add keymaps to the functions in iron.core
+                keymaps = {
+                    send_motion = "<space>ic",
+                    visual_send = "<space>ic",
+                    send_file = "<space>if",
+                    send_line = "<space>il",
+                    send_paragraph = "<space>ip",
+                    send_until_cursor = "<space>iu",
+                    cr = "<space>i<cr>",
+                    interrupt = "<space>i<space>",
+                    exit = "<space>iq",
+                    clear = "<space>il",
+                },
+                -- If the highlight is on, you can change how it looks
+                -- For the available options, check nvim_set_hl
+                highlight = {
+                    italic = true
+                },
+                ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+            }
+
+            -- iron also has a list of commands, see :h iron-commands for all available commands
+            vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
+            vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
+            vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+            vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
         end,
+    },
+    {
+        "GCBallesteros/jupytext.nvim",
+        -- config = true,
+        -- Depending on your nvim distro or config you may need to make the loading not lazy
+        -- lazy=false,
+        opts = {
+            style = "hydrogen",
+            output_extension = "auto", -- Default extension. Don't change unless you know what you are doing
+            force_ft = nil,            -- Default filetype. Don't change unless you know what you are doing
+            custom_language_formatting = {
+                python = {
+                    extension = "md",
+                    style = "markdown",
+                    force_ft = "markdown", -- you can set whatever filetype you want here
+                },
+            }
+        },
     },
 }
