43df3175a36967573ddd6ef3ac7aaf173f7c2e5e
mtoon <77199166+mtoons@users.noreply.github.com>
2024-11-20 15:33:30 +0100

    sessions keyboard

--- nvim/lua/plugins/sessions.lua
+++ nvim/lua/plugins/sessions.lua
@@ -3,6 +3,9 @@ return {
         "olimorris/persisted.nvim",
         lazy = false, -- make sure the plugin is always loaded at startup
         config = true,
+        opts = {
+            autostart = false,
+        },
         dependencies = {
             "nvim-telescope/telescope.nvim"
         },
@@ -10,8 +13,16 @@ return {
             require("telescope").load_extension("persisted")
             vim.keymap.set('n', '<leader>ss', ":Telescope persisted theme=dropdown<CR>",
                 { desc = '[S]earch [S]essions', silent = true, })
+            vim.keymap.set('n', '<leader>Ss', ":SessionStart<CR>",
+                { desc = '[S]essions [S]tart', silent = true, })
+            vim.keymap.set('n', '<leader>Sb', ":SessionStop<CR>",
+                { desc = '[S]essions [B]reak', silent = true, })
+            vim.keymap.set('n', '<leader>Sl', ":SessionLoad<CR>",
+                { desc = '[S]essions [L]oad', silent = true, })
+            vim.keymap.set('n', '<leader>Sd', ":SessionDelete<CR>",
+                { desc = '[S]essions [D]elete', silent = true, })
         end,
         -- keys = {
-        -- }
+        -- },
     }
 }
