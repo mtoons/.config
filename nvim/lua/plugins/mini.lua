3865282494f8ccd50c7b948f0203a9c442790722
mtoon <77199166+mtoons@users.noreply.github.com>
2025-11-12 20:13:40 +0100

    remove mini ai

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,36 +1,9 @@
 return {
-    -- {
-    --     'echasnovski/mini.nvim',
-    --     version = false,
-    --     opts = {
-    --         ai = {
-    --             silent = true,
-    --         },
-    --         surround = {
-    --             silent = true,
-    --         },
-    --         icons = {
-    --             style = "glyph",
-    --         },
-    --     },
-    --     config = function(_, opts)
-    --         for module, config in pairs(opts) do
-    --             require("mini." .. module).setup(config)
-    --         end
-    --     end,
-    -- },
     {
         'echasnovski/mini.icons',
         version = false,
         opts = {
             style = 'glyph',
-        },
-    },
-    {
-        'echasnovski/mini.ai',
-        version = false,
-        opts = {
-            silent = true,
         },
     },
 }
